c4d28a1129df8cd18dda56314547635e2595472f
sxiao1 <sxiao1@stuy.edu>
2018-09-15 23:45:59 -0400

    second commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public static void main(String[] args) {
+    String x = " Sabrina";
+    String y = " What's up!";
+    System.out.println( "Hello my name is" + x + y );
+  }
+}
